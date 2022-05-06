0413442836de4fd7bb8c5374cc3bc742e72d63ff
Steve Zatz <slzatz@gmail.com>
2022-05-06 09:17:16 -0400

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/slzatz/images
+
+go 1.18
+
+require github.com/dropbox/godropbox v0.0.0-20200228041828-52ad444d3502 // indirect
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/dropbox/godropbox/container/bitvector"
+	"github.com/slzatz/images/bmp1bit"
+)
+
+func main() {
+
+	fname := os.Args[1]
+	name, _, _ := strings.Cut(fname, ".")
+	cmd := exec.Command("convert", fname, "-resize", "296x400^", "-gravity", "North", "-extent", "296x400",
+		"-depth", "1", "-type", "bilevel", "BMP3:"+name+".bmp")
+	cmd.Dir = "/home/slzatz/code/images/pictures"
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("ImageMagick convert error: %v\n", err)
+	}
+
+	file, _ := os.Open("pictures/" + name + ".bmp")
+	defer file.Close()
+
+	img, err := bmp1bit.Decode(file)
+	if err != nil {
+		log.Fatalf("Decode error: %v\n", err)
+	}
+
+	z, ok := img.(*image.Paletted)
+	if !ok {
+		log.Fatalf("Image is not Paletted")
+	}
+
+	fmt.Printf("Paletted\n")
+	fmt.Printf("Number of pixels = %d\n", len(z.Pix))
+	fmt.Printf("Stride = %v\n", z.Stride)
+	fmt.Printf("Rect Min X = %v\n", z.Rect.Min.X)
+	fmt.Printf("Rect Min Y = %v\n", z.Rect.Min.Y)
+	fmt.Printf("Rect Max X = %v\n", z.Rect.Max.X)
+	fmt.Printf("Rect Max Y = %v\n", z.Rect.Max.Y)
+	bv := bitvector.NewBitVector([]byte{}, 0)
+	for _, v := range z.Pix {
+		bv.Append(v)
+	}
+	os.WriteFile("pictures/"+name+".bz", bv.Bytes(), 0644)
+}
